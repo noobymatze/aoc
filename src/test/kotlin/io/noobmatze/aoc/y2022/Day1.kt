5300635a5e754fe82dfc4f9421f55ca62c75336c
Matthias Metzger <noobymatze@yahoo.de>
2022-12-01 06:47:36 +0100

    do day1

--- src/test/kotlin/io/noobmatze/aoc/y2022/Day1.kt
+++ src/test/kotlin/io/noobmatze/aoc/y2022/Day1.kt
@@ -7,8 +7,20 @@ class Day1 {
 
     @Test
     fun test() {
-        val result = Aoc.getInput(1)
+        val result = Aoc.getInput(1).split("\n\n")
+            .maxOf { it.lines().sumOf(String::toInt) }
+
         println(result)
+    }
+
+    @Test
+    fun test2() {
+        val result = Aoc.getInput(1).split("\n\n")
+            .map { it.lines().sumOf(String::toInt) }
+            .sortedDescending()
+            .take(3)
+
+        println(result.sum())
     }
 
 }
