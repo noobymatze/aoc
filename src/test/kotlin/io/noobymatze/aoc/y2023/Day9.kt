bc81e6a8d8582c475808268caf12b0d8f149b99a
Matthias Metzger <noobymatze@yahoo.de>
2023-12-09 15:00:15 +0100

    clean up day 9

--- src/test/kotlin/io/noobymatze/aoc/y2023/Day9.kt
+++ src/test/kotlin/io/noobymatze/aoc/y2023/Day9.kt
@@ -1,9 +1,6 @@
 package io.noobymatze.aoc.y2023
 
 import io.noobymatze.aoc.Aoc
-import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 import kotlin.test.Test
 
 class Day9 {
@@ -13,27 +10,14 @@ class Day9 {
         val result = Aoc.getInput(9)
             .lineSequence()
             .map { it.trim().split(" ").map { it.toInt() } }
-            .map {
-                val values = mutableListOf(it.toMutableList())
-                while (!values.last().all { it == 0 }) {
-                    val list = values.last().zipWithNext().map {
-                        it.second - it.first
-                    }.toMutableList()
-                    values.add(list)
-                }
-
-                values.removeLast()
-                var v = 0L
-                var i = values.size - 1
-                while (i >= 0) {
-                    v += values[i].last()
-                    i--
-                }
-                v
+            .map { history ->
+                generateSequence(history) { it.zipWithNext { a, b -> b - a } }
+                    .takeWhile { it.any { it != 0 } }
+                    .sumOf { it.last() }
             }
-            .toList()
+            .sum()
 
-        println(result.sum())
+        println(result)
     }
 
     @Test
@@ -41,27 +25,14 @@ class Day9 {
         val result = Aoc.getInput(9)
             .lineSequence()
             .map { it.trim().split(" ").map { it.toInt() } }
-            .map {
-                val values = mutableListOf(it.toMutableList())
-                while (!values.last().all { it == 0 }) {
-                    val list = values.last().zipWithNext().map {
-                        it.first - it.second
-                    }.toMutableList()
-                    values.add(list)
-                }
-
-                values.removeLast()
-                var v = 0L
-                var i = values.size - 1
-                while (i >= 0) {
-                    v += values[i].first()
-                    i--
-                }
-                v
+            .map { history ->
+                generateSequence(history) { it.zipWithNext { a, b -> a - b } }
+                    .takeWhile { it.any { it != 0 } }
+                    .sumOf { it.first() }
             }
-            .toList()
+            .sum()
 
-        println(result.sum())
+        println(result)
     }
 
 }
